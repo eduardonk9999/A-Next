722cde157d2beb808d3f0d0378a1d13049b5f0fa
eduardonk9999 <thiago.novak7@gmail.com>
2022-03-28 18:48:19 -0300

    Add, aulas

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function FAQPags() {
+  return(
+    <div>
+      <h1>FFAQ</h1>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function HomePage() {
+  return(
+    <div>
+      <h1>Alura Case!</h1>
+      <Link href="/faq">FAQ</Link>
+    </div>
+  )
+}
